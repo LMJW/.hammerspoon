c57937d89ef0656e7d89fb1b590292a8b3107726
LMJW <heysuperming@gmail.com>
2020-09-16 23:49:41 +1000

    Add Time for different time zone

---
+++ init.lua
@@ -0,0 +1,20 @@
+caffeine = hs.menubar.new()
+function setCaffeineDisplay(state)
+    time = hs.timer.localTime()
+    hour = time // 3600 --SYD
+    minutes = (time % 3600) // 60
+    caffeine:setTitle(
+        string.format(
+            "UTC %02d:%02d | SYD %02d:%02d | SEA %02d:%02d | DUB %02d:%02d "
+            ,hour-10, minutes, hour, minutes, (hour+7)%24, minutes, (hour+15)%24, minutes))
+end
+
+function caffeineClicked()
+    setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
+end
+
+if caffeine then
+    caffeine:setClickCallback(caffeineClicked)
+    setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
+    hs.timer.doEvery(60, caffeineClicked)
+end
