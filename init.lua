a914338be4c08d625d272c8bead7b75272461e4c
LMJW <heysuperming@gmail.com>
2020-09-17 10:44:21 +1000

    Add New york time

--- init.lua
+++ init.lua
@@ -5,8 +5,9 @@ function setCaffeineDisplay(state)
     minutes = (time % 3600) // 60
     caffeine:setTitle(
         string.format(
-            "UTC %02d:%02d | SYD %02d:%02d | SEA %02d:%02d | DUB %02d:%02d "
-            ,hour-10, minutes, hour, minutes, (hour+7)%24, minutes, (hour+15)%24, minutes))
+            "UTC %02d:%02d | SYD %02d:%02d | SEA %02d:%02d | NY %02d:%02d | DUB %02d:%02d ",
+            (hour+14)%24, minutes, hour, minutes, (hour+7)%24, minutes,
+            (hour+10)%24, minutes, (hour+15)%24, minutes))
 end
 
 function caffeineClicked()
