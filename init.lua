0e4cd61e9be0c650b987e666cce80c1759f6ed88
LMJW <heysuperming@gmail.com>
2020-11-12 14:57:41 +1100

    Add clipboard tool (keep 64 records)

--- init.lua
+++ init.lua
@@ -1,13 +1,15 @@
+require "clipboard"
 caffeine = hs.menubar.new()
 function setCaffeineDisplay(state)
     time = hs.timer.localTime()
     hour = time // 3600 --SYD
     minutes = (time % 3600) // 60
+    dls = 1 -- day light saving
     caffeine:setTitle(
         string.format(
             "UTC %02d:%02d | SYD %02d:%02d | SEA %02d:%02d | NY %02d:%02d | DUB %02d:%02d ",
-            (hour+14)%24, minutes, hour, minutes, (hour+7)%24, minutes,
-            (hour+10)%24, minutes, (hour+15)%24, minutes))
+            (hour+14-dls)%24, minutes, hour, minutes, (hour+7-dls)%24, minutes,
+            (hour+10-dls)%24, minutes, (hour+15-dls)%24, minutes))
 end
 
 function caffeineClicked()
